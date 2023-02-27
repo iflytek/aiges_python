d16ac0482ea793ae667681c327c7c53ef60c8db6
ybyang <ybyang7@iflytek.com>
2023-02-27 14:48:17 +0800

    feat:

--- aiges/dto.py
+++ aiges/dto.py
@@ -59,10 +59,27 @@ class DataListCls:
 class ResponseData:
     def __init__(self, key='', data='', length=0, type=DataText, status=Once):
         self.key = key
-        self.data = data
-        self.len = length
+        self._len = 0
         self.type = type
         self.status = status
+        self._data = b""
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def len(self):
+        return self._len
+
+    def setDataType(self, t):
+        if not t in [DataText, DataAudio, DataImage, DataVideo]:
+            raise Exception("Please specify the correct data type...")
+        self.type = t
+
+    def setData(self, d):
+        self._data = d
+        self._len = len(d)
 
     def toJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__,
@@ -92,12 +109,15 @@ class SessionCreateResponse:
         return json.dumps(self, default=lambda o: o.__dict__,
                           sort_keys=True, indent=4)
 
+
 result_Queue = None
+
 
 def init_rq():
     global result_Queue
     result_Queue = queue.Queue()
     return result_Queue
+
 
 def callback(r: Response, sid: str):
     global result_Queue
@@ -105,8 +125,9 @@ def callback(r: Response, sid: str):
         raise Exception("you should execute init_rq first")
     for nod in r.list:
         print("sid %s, get callback: %s: len %d, type: %s, status: %s" % (
-        sid, nod.key, nod.len, Types[nod.type], Status[nod.status]))
+            sid, nod.key, nod.len, Types[nod.type], Status[nod.status]))
     result_Queue.put(r)
+
 
 if __name__ == '__main__':
     t = Response()
